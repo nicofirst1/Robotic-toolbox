146944f35a30f5b67f6865e999568fc70c3495c1
dizzi <nicofirst1@gmail.com>
2019-10-09 18:27:59 +0200

    fixed angular part

--- jacobians_stuff/jac_angular.m
+++ jacobians_stuff/jac_angular.m
@@ -1,44 +1,35 @@
 function jac_a = jac_angular(robot)
 %Compute the angular jacobian for revolutive joints
 
-z0=[0;0;1];
 rotations=homo_mat(robot);
-% discard tool rotation
+% discard tool homo
 rotations=rotations(:,:,1:robot.ndof);
+% get the rotations 
 rotations=rotations(1:3,1:3,:);
+% initialize the jacobian 
 jac_a=sym(zeros(3,length(rotations)));
 
-zero=[0;0;0];
 
 if robot.type(1)=='R'
-
-    jac_a(:,1)=z0;
+    % if the joint is prismatic the azis of rotation is Z
+    z0=[0;0;1];
 else
-        jac_a(:,1)=zero;
+    % else there is no rotation
+    z0=[0;0;0];
+
 end
 
+% for every frame multiply the rotation matrix
+for idx=1:size(rotations,3)
+    zi=1;
 
-for idx=2:length(rotations)
-   zi=1;
-
-   for jdx=1:(idx-1)
-       zi=zi*rotations(:,:,jdx);
-   end
+    for jdx=1:idx
+        zi=zi*rotations(:,:,jdx);
+    end
    
-   zi=zi*z0;
-   if robot.type(idx)=='R'
+    zi=zi*z0;
+    
     jac_a(:,idx)=zi;
-   else
-     jac_a(:,idx)=zero;
-   end
-
-    
-    
-    
-    
-end
-jac_a=simplify(jac_a);
-
-
 end
 
+end
